86e91d9828463f3c34cf9ff37eee923373da0e55
wangxing <18912540921@163.com>
2017-05-10 16:56:50 +0800

    implement current_cart function and add_product_to_cart btn

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,7 +4,7 @@ class Cart < ApplicationRecord
 
   def add_product_to_cart(product)
     ci = cart_items.build
-    ci = product = product
+    ci.product = product
     ci.quantity = 1
     ci.save
   end
