113569dc802c28728e58a5b50956fa59afeede3f
wangxing <18912540921@163.com>
2017-05-17 16:50:29 +0800

    add payment_method function

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,11 @@ root 'products#index'
   end
 
   resources :cart_items
-  resources :orders
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end
 
 end
